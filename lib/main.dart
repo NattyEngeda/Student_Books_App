256b5ef51817209a9c7c1decaa50d054de3b2fc7
Naatty123 <nattynengeda@gmail.com>
2022-10-03 14:44:12 +0300

    Animation Complete Half Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'lists.dart';
 import 'subjectspage.dart';
 import 'test/test.dart';
@@ -18,12 +19,150 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      home: Home(),
       theme: ThemeData(primarySwatch: Colors.teal),
     );
   }
 }
 
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    Grades grades = new Grades();
+    double _w = MediaQuery.of(context).size.width;
+    int columnCount = 2;
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Header'),
+          centerTitle: false,
+        ),
+        body: AnimationLimiter(
+          child: GridView.count(
+              physics: BouncingScrollPhysics(
+                  parent: AlwaysScrollableScrollPhysics()),
+              padding: EdgeInsets.fromLTRB(_w / 60, _w / 60, _w / 60, _w / 60),
+              crossAxisCount: columnCount,
+              children: List.generate(grades.grade.length, (int index) {
+                return AnimationConfiguration.staggeredGrid(
+                  position: index,
+                  duration: Duration(milliseconds: 500),
+                  columnCount: columnCount,
+                  child: ScaleAnimation(
+                    duration: Duration(milliseconds: 900),
+                    curve: Curves.fastLinearToSlowEaseIn,
+                    child: FadeInAnimation(
+                        child: GestureDetector(
+                      child: GradeBox(
+                        grades: grades.grade[index],
+                      ),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SubjectsPage(
+                                      ggrade: grades.grade[index],
+                                    )));
+                      },
+                    )),
+                  ),
+                );
+              })),
+        ));
+  }
+}
+
+class GradeBox extends StatelessWidget {
+  const GradeBox({Key? key, required this.grades}) : super(key: key);
+  final String grades;
+
+  @override
+  Widget build(BuildContext context) {
+    double _w = MediaQuery.of(context).size.width;
+    return Container(
+      child: Center(
+          child: Text(
+        grades,
+        style: TextStyle(fontSize: 30),
+      )),
+      margin: EdgeInsets.only(bottom: _w / 30, left: _w / 60, right: _w / 60),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 40,
+            spreadRadius: 10,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// Flutter GridView
+class GridView1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double _w = MediaQuery.of(context).size.width;
+    int columnCount = 3;
+
+    return Scaffold(
+      appBar: AppBar(
+          title: Text("Go Back"),
+          centerTitle: true,
+          brightness: Brightness.dark),
+      body: AnimationLimiter(
+        child: GridView.count(
+          physics:
+              BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+          padding: EdgeInsets.all(_w / 60),
+          crossAxisCount: columnCount,
+          children: List.generate(
+            50,
+            (int index) {
+              return AnimationConfiguration.staggeredGrid(
+                position: index,
+                duration: Duration(milliseconds: 500),
+                columnCount: columnCount,
+                child: ScaleAnimation(
+                  duration: Duration(milliseconds: 900),
+                  curve: Curves.fastLinearToSlowEaseIn,
+                  child: FadeInAnimation(
+                    child: Container(
+                      margin: EdgeInsets.only(
+                          bottom: _w / 30, left: _w / 60, right: _w / 60),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.1),
+                            blurRadius: 40,
+                            spreadRadius: 10,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// Old Home
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -47,7 +186,7 @@ class _HomePageState extends State<HomePage> {
               crossAxisCount: 2, crossAxisSpacing: 4.0, mainAxisSpacing: 4.0),
           itemBuilder: (BuildContext context, int index) {
             return GestureDetector(
-              child: GradeBox(grades: grades.grade[index]),
+              child: OldGradeBox(grades: grades.grade[index]),
               onTap: () {
                 Navigator.push(
                     context,
@@ -62,8 +201,66 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-class GradeBox extends StatelessWidget {
-  const GradeBox({Key? key, required this.grades}) : super(key: key);
+// Old List View
+
+class SlideAnimation1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double _w = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+          title: Text("Go Back"),
+          centerTitle: true,
+          brightness: Brightness.dark),
+      body: AnimationLimiter(
+        child: ListView.builder(
+          padding: EdgeInsets.all(_w / 30),
+          physics:
+              BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+          itemCount: 20,
+          itemBuilder: (BuildContext c, int i) {
+            return AnimationConfiguration.staggeredList(
+              position: i,
+              delay: Duration(milliseconds: 100),
+              child: SlideAnimation(
+                duration: Duration(milliseconds: 2500),
+                curve: Curves.fastLinearToSlowEaseIn,
+                horizontalOffset: 30,
+                verticalOffset: 300.0,
+                child: FlipAnimation(
+                  duration: Duration(milliseconds: 3000),
+                  curve: Curves.fastLinearToSlowEaseIn,
+                  flipAxis: FlipAxis.y,
+                  child: Container(
+                    margin: EdgeInsets.only(bottom: _w / 20),
+                    height: _w / 4,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          blurRadius: 40,
+                          spreadRadius: 10,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+// Old Grade Box
+class OldGradeBox extends StatelessWidget {
+  const OldGradeBox({Key? key, required this.grades}) : super(key: key);
   final String grades;
   @override
   Widget build(BuildContext context) {
--- lib/subjectspage.dart
+++ lib/subjectspage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'chapterspage.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
 class Subject {
   final String subjects;
@@ -16,7 +17,6 @@ class SubjectsPage extends StatelessWidget {
 
     switch (ggrade) {
       case "Grade 12":
-        subs.add(Subject("Grade 12"));
         subs.add(Subject("English"));
         subs.add(Subject("Math"));
         subs.add(Subject("ICT"));
@@ -100,28 +100,42 @@ class SubjectsPage extends StatelessWidget {
 
     double _w = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(ggrade),
-      ),
-      body: ListView.builder(
-        padding: EdgeInsets.fromLTRB(5, 15, 5, 10),
-        itemCount: subbs.length,
-        itemBuilder: (BuildContext context, int index) {
-          return GestureDetector(
-            child: SubjectBox(subname: subbs[index]),
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => ChaptersPage(
-                            subject: subbs[index],
-                            grade: ggrade,
-                          )));
+        appBar: AppBar(
+          title: Text(ggrade),
+        ),
+        body: AnimationLimiter(
+          child: ListView.builder(
+            padding: EdgeInsets.all(_w / 30),
+            physics:
+                BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+            itemCount: subbs.length,
+            itemBuilder: (BuildContext context, int index) {
+              return AnimationConfiguration.staggeredList(
+                  position: index,
+                  duration: Duration(milliseconds: 100),
+                  child: SlideAnimation(
+                      duration: Duration(milliseconds: 1000),
+                      curve: Curves.fastLinearToSlowEaseIn,
+                      // horizontalOffset: 30,
+                      // verticalOffset: 300.0,
+                      child: FadeInAnimation(
+                        duration: Duration(milliseconds: 1000),
+                        child: GestureDetector(
+                          child: SubjectBox(subname: subbs[index]),
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => ChaptersPage(
+                                          subject: subbs[index],
+                                          grade: ggrade,
+                                        )));
+                          },
+                        ),
+                      )));
             },
-          );
-        },
-      ),
-    );
+          ),
+        ));
   }
 }
 
@@ -132,32 +146,57 @@ class SubjectBox extends StatelessWidget {
   Widget build(BuildContext context) {
     double _w = MediaQuery.of(context).size.width;
     return Container(
+        margin: EdgeInsets.only(bottom: _w / 20),
+        height: _w / 3,
         decoration: BoxDecoration(
-            border: Border.all(width: 1.0, color: Colors.black),
-            borderRadius: BorderRadius.all(Radius.circular(20)),
-            boxShadow: const [
-              BoxShadow(
-                  color: Colors.grey,
-                  offset: Offset(3, 3),
-                  blurRadius: 10,
-                  spreadRadius: 1),
-              BoxShadow(
-                color: Colors.white,
-                offset: const Offset(0.0, 0.0),
-                blurRadius: 0.0,
-                spreadRadius: 0.0,
-              ), //Box
-            ]),
-        margin:
-            EdgeInsets.only(bottom: _w / 50, left: _w / 100, right: _w / 100),
-        width: _w,
-        height: 120,
+          color: Colors.white,
+          borderRadius: BorderRadius.all(
+            Radius.circular(20),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 40,
+              spreadRadius: 10,
+            ),
+          ],
+        ),
         child: Center(
           child: Text(
             this.subname.subjects,
             textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 40),
+            style: TextStyle(fontSize: 35),
           ),
         ));
   }
 }
+
+// Old COntainer
+//  Container(
+//         decoration: BoxDecoration(
+//             border: Border.all(width: 1.0, color: Colors.black),
+//             borderRadius: BorderRadius.all(Radius.circular(20)),
+//             boxShadow: const [
+//               BoxShadow(
+//                   color: Colors.grey,
+//                   offset: Offset(3, 3),
+//                   blurRadius: 10,
+//                   spreadRadius: 1),
+//               BoxShadow(
+//                 color: Colors.white,
+//                 offset: const Offset(0.0, 0.0),
+//                 blurRadius: 0.0,
+//                 spreadRadius: 0.0,
+//               ), //Box
+//             ]),
+//         margin:
+//             EdgeInsets.only(bottom: _w / 50, left: _w / 100, right: _w / 100),
+//         width: _w,
+//         height: 120,
+//         child: Center(
+//           child: Text(
+//             this.subname.subjects,
+//             textAlign: TextAlign.center,
+//             style: TextStyle(fontSize: 40),
+//           ),
+//         ));
